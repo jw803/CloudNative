a8243a3f034b8a6079bb0101fe65a59f6ba03d5e
paul <jw80301@gmail.com>
2021-12-12 22:03:15 +0800

    finished http metrics

---
+++ module10/go.mod
@@ -0,0 +1,8 @@
+module github.com/jw803/module10
+
+go 1.16
+
+require (
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+)
---
+++ module10/metrics/metrics.go
@@ -0,0 +1,68 @@
+package metrics
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+
+)
+
+func Register() {
+	err := prometheus.Register(functionLatency)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+const (
+	MetricsNamespace = "httpserver"
+)
+
+// NewExecutionTimer provides a timer for Updater's RunOnce execution
+func NewTimer() *ExecutionTimer {
+	return NewExecutionTimer(functionLatency)
+}
+
+var (
+	functionLatency = CreateExecutionTimeMetric(MetricsNamespace,
+		"Time spent.")
+
+)
+
+// NewExecutionTimer provides a timer for admission latency; call ObserveXXX() on it to measure
+//定義了一個histo直方圖的指標
+func NewExecutionTimer(histo *prometheus.HistogramVec) *ExecutionTimer {
+	now := time.Now()
+	return &ExecutionTimer{
+		histo: histo,
+		start: now,
+		last:  now,
+	}
+}
+
+// ObserveTotal measures the execution time from the creation of the ExecutionTimer
+func (t *ExecutionTimer) ObserveTotal() {
+	(*t.histo).WithLabelValues("total").Observe(time.Now().Sub(t.start).Seconds())
+}
+
+// CreateExecutionTimeMetric prepares a new histogram labeled with execution step
+// 指數及上升的bucket 2的刺方擴展，並統計前15 bucket的值
+func CreateExecutionTimeMetric(namespace string, help string) *prometheus.HistogramVec {
+	return prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: namespace,
+			Name:      "execution_latency_seconds",
+			Help:      help,
+			Buckets: prometheus.ExponentialBuckets(0.001, 2, 15),
+		}, []string{"step"},
+	)
+}
+
+// ExecutionTimer measures execution time of a computation, split into major steps
+// usual usage pattern is: timer := NewExecutionTimer(...) ; compute ; timer.ObserveStep() ; ... ; timer.ObserveTotal()
+type ExecutionTimer struct {
+	histo *prometheus.HistogramVec
+	start time.Time
+	last  time.Time
+}
